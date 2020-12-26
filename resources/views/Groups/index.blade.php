65057f8be34037df52b056bb11e185b8ce280757
rizkayuliyantidewi <72368819+rizkayuliyantidewi@users.noreply.github.com>
2020-12-26 21:25:08 +0700

    commit 7
    commit 7

--- resources/views/Groups/index.blade.php
+++ resources/views/Groups/index.blade.php
@@ -14,10 +14,21 @@
     <p class="card-text">{{ $group['description'] }}</p>
 
     <hr>
-    <a href="" class="card-link btn-primary">Tambah anggota teman</a>
+    <a href="/groups/addmember/{{$group['id']}}" class="card-link btn-primary">Tambah anggota teman</a>
+    <ul class="list-group">
 @foreach ($group->friends as $friend)
-<li> {{$friend->nama}} </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    {{$friend->nama}}
+    <form action="/groups/deleteaddmember/{{$friend->id}}" method="POST">
+    @csrf
+      @method('PUT')
+        <button type="submit"class="bedge card-link btn-danger">x</button>
+        </form>
+  </li>
+
 @endforeach
+</ul>
     <hr>
     
     <a href="//groups/{{$group['id']}}/edit" class="card-link btn-warning">Edit group</a>
